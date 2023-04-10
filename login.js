3659dd28500f3e78e3c923108829bcee6f16e9b0
VitorJosee <95343929+VitorJosee@users.noreply.github.com>
2023-04-09 21:20:54 -0300

    tela de cadastro

--- login.js
+++ login.js
@@ -1,35 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
 
 export default function App() {
 
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+ 
 
-  const cadastro = () =>{
-    alert (nome);
-    alert(email);
-    alert(senha);  
-  }
   
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      
-      <Image style={{width:256,height:256}} source={require('./assets/wz2.ico')}/>
-
-      <Text style={styles.paragraph}>Nome:</Text>
-      <TextInput style={styles.TextInput} onChangeText={text=>setNome(nome)} />
-      <Text style={styles.paragraph}>Email:</Text>
-      <TextInput style={styles.TextInput} onChangeText={text=>setEmail(email)} />
-      <Text style={styles.paragraph}>Senha:</Text>
-      <TextInput secureTextEntry={true} style={styles.TextInput} onChangeText={text=>setSenha(senha)} /> 
-
-      <TouchableOpacity style={styles.botao} onPress={()=>cadastro()}>
-        <Text style={styles.textr}>CADASTRAR!</Text>
-      </TouchableOpacity>
 
     </View>
   );
